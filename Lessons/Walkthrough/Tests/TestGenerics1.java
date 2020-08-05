78403f061124530285339cfe3d8fe2604d7f6d6e
apalk <apalk@192.168.86.133>
2020-08-05 11:35:35 -0700

    yay more stuff

---
+++ Lessons/Walkthrough/Tests/TestGenerics1.java
@@ -0,0 +1,43 @@
+package Tests;
+
+import java.util.*;
+
+public class TestGenerics1 {
+	public static void main(String[] args) {
+		new TestGenerics1().go();
+	}
+	
+	public void go() {
+		ArrayList<Animal> animals = new ArrayList<Animal>();
+		animals.add(new Dog());
+		animals.add(new Cat());
+		animals.add(new Dog());
+		
+		takeAnimals(animals);
+		
+		ArrayList<Dog> dogs = new ArrayList<Dog>();
+		dogs.add(new Dog());
+		dogs.add(new Dog());
+		takeAnimals(dogs);
+	}
+	
+	public void takeAnimals(ArrayList<? extends Animal> animals) {
+		for(Animal a: animals) {
+			a.eat();
+		}
+	}
+}
+
+abstract class Animal {
+	void eat() {
+		System.out.println("animal eating");
+	}
+}
+
+class Dog extends Animal {
+	void bark() { }
+}
+
+class Cat extends Animal {
+	void meow() { }
+}
---
+++ Lessons/src/ReverseEngineer/SortMountains.java
@@ -0,0 +1,49 @@
+//package ReverseEngineer;
+//
+//import _;
+//
+//public class SortMountains {
+//	
+//	LinkedList_ mtn = new LinkedList_();
+//	
+//	class NameCompare _ {
+//		public int compare(Mountain one, Mountain two) {
+//			return _;
+//		}
+//	}
+//	class HightCompare _ {
+//		public int compare(Mountain one, Mountain two) {
+//			return (_);
+//		}
+//	}
+//	public static void main(String[] args) {
+//		new SortMountains().go();
+//	}
+//	public void go() {
+//		mtn.add(new Mountain("Longs", 14255));
+//		mtn.add(new Mountain("Elbert", 14433));
+//		mtn.add(new Mountain("Maroon", 14156));
+//		mtn.add(new Mountain("Castle", 14265));
+//		
+//		System.out.println("as entered: \n" + mtn);
+//		NameCompare nc = new NameCompare();
+//		_;
+//		System.out.println("by name:\n" + mtn);
+//		HeightCompare hc = new HeightCompare();
+//		_;
+//		System.out.println("bt height:\n" + mtn);
+//	}
+//}
+//
+//class Mountain {
+//	_;
+//	_;
+//	
+//	_ {
+//		_;
+//		_;
+//	}
+//	_ {
+//		_;
+//	}
+//}
