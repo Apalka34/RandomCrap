45db178ed8ff88e1847436690e830c0ad31c0ebc
apalk <apalk@192.168.86.37>
2020-12-22 18:58:53 -0800

    Correct Not working thing

--- Forge3/src/main/java/util/RegistryHandler.java
+++ Forge3/src/main/java/util/RegistryHandler.java
@@ -4,19 +4,19 @@ import com.example.examplemod.ExampleMod;
 import Items.ItemBase;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
-//import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class RegistryHandler {
 	
-//	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ExampleMod.MOD_ID);
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ExampleMod.MOD_ID);
 			
-//	public static void init() {
-//		ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+	public static void init() {
+		ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
 	}
 	
-	//My Items
+//	My Items
 	public static final RegistryObject<Item> Stupidity = ITEMS.register("Stupidity", ItemBase::new);
 
 }
