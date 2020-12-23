4ae5a73d7a4041b11d45e1c7c0468ae9448d8d7f
apalk <apalk@192.168.86.37>
2020-12-22 18:57:15 -0800

    Not working

--- Forge3/src/main/java/com/example/examplemod/ExampleMod.java
+++ Forge3/src/main/java/com/example/examplemod/ExampleMod.java
@@ -5,6 +5,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import util.RegistryHandler;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,10 +14,13 @@ import org.apache.logging.log4j.Logger;
 public class ExampleMod
 {
     private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "examplemod";
 
     public ExampleMod() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        
+        RegistryHandler.init();
 
         MinecraftForge.EVENT_BUS.register(this);
     }
---
+++ Forge3/src/main/java/util/RegistryHandler.java
@@ -0,0 +1,22 @@
+package util;
+
+import com.example.examplemod.ExampleMod;
+import Items.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+	
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ExampleMod.MOD_ID);
+			
+	public static void init() {
+		ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+	}
+	
+	//My Items
+	public static final RegistryObject<Item> Stupidity = ITEMS.register("Stupidity", ItemBase::new);
+
+}
