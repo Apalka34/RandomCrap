c3619d2d9990829cba5659a946ad22e5b07bf92c
apalk <apalk@192.168.86.37>
2020-12-22 18:57:31 -0800

    Continue to not working

--- Forge3/src/main/java/util/RegistryHandler.java
+++ Forge3/src/main/java/util/RegistryHandler.java
@@ -4,16 +4,16 @@ import com.example.examplemod.ExampleMod;
 import Items.ItemBase;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+//import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class RegistryHandler {
 	
-	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ExampleMod.MOD_ID);
+//	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ExampleMod.MOD_ID);
 			
-	public static void init() {
-		ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+//	public static void init() {
+//		ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
 	}
 	
 	//My Items
