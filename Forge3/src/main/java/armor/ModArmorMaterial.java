a0349ed5df4e57c1fad6fd40bcad83babc8a333e
apalk <apalk@192.168.86.37>
2020-12-24 17:49:12 -0800

    Need to update eclipse.

---
+++ Forge3/src/main/java/armor/ModArmorMaterial.java
@@ -0,0 +1,79 @@
+package armor;
+
+import java.util.function.Supplier;
+import com.example.examplemod.ExampleMod;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import util.RegistryHandler;
+
+public enum ModArmorMaterial implements IArmorMaterial {
+	
+	STUPIDITY(ExampleMod.MOD_ID + ":stupidity", 1, new int[] {2147483647, 2147483647, 2147483647, 2147483647}, 2147483647,
+			SoundEvent.ITEM_ARMOR_EQUIP_GENERIC, 2147483647.0f, () -> {return Ingredient.fromItems(RegistryHandler.Stupidity.get());}, 2147483647);
+	
+	private static final int[] MAX_DAMAGE_ARRAY = new int[] {1, 1, 1, 1};
+	private final String name;
+	private final int maxDamageFactor;
+	private final int[] damageReductionAmountArray;
+	private final int enchantability;
+	private final SoundEvent soundEvent;
+	private final float toughness;
+	private final Supplier<Ingredient> repairMaterial;
+	private final float knockbackResistance;
+	
+	ModArmorMaterial(String name, int maxDamageFactor, int[] damageReductionAmountArray, int enchantability, SoundEvent soundEvent, float toughness,
+	Supplier<Ingredient> repairMaterial) {
+		this.name = name;
+		this.maxDamageFactor = maxDamageFactor;
+		this.damageReductionAmountArray = damageReductionAmountArray;
+		this.enchantability = enchantability;
+		this.soundEvent = soundEvent;
+		this.toughness = toughness;
+		this.repairMaterial = repairMaterial;
+		this.knockbackResistance = knockbackResistance;
+		
+	}
+
+	@Override
+	public int getDurability(EquipmentSlotType slotIn) {
+		return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * maxDamageFactor;
+	}
+
+	@Override
+	public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+		return this.getDamageReductionAmount(slotIn.getIndex());
+	}
+
+	@Override
+	public int getEnchantability() {
+		return this.enchantability;
+	}
+
+	@Override
+	public SoundEvent getSoundEvent() {
+		return this.soundEvent;
+	}
+
+	@Override
+	public Ingredient getRepairMaterial() {
+		return this.repairMaterial.get();
+	}
+
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	@Override
+	public float getToughness() {
+		return this.toughness;
+	}
+
+	@Override
+	public float getKnockbackResistance() {
+		return this.knockbackResistance;
+	}
+}
